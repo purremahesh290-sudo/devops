4150e6bc990d558d5ae4ea37e031d7bf08d03c82
purremahesh290 <purremahesh290@gmail.com>
2026-04-02 09:56:59 +0100

    test 1

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -75,7 +75,7 @@ const Login = () => {
           <h1 className="login-hero-title">ConstructTrack</h1>
           <p className="login-hero-subtitle">
             Your all-in-one construction project management solution.
-            Track progress, manage tasks, and keep your projects on schedule and under budget hi.
+            Track progress, manage tasks, and keep your projects on schedule and under budget.
           </p>
         </div>
       </div>
